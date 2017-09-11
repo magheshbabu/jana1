b7b7f213263548cbcc8b3b04744d6d20263c7854
magheshbabu <maghesh@gmail.com>
2017-09-12 00:37:45 +0530

    added mal calendar

--- src/main/webapp/i18n/angular-locale_ta.js
+++ src/main/webapp/i18n/angular-locale_ta.js
@@ -1,101 +1,119 @@
 'use strict';
 angular.module("ngLocale", [], ["$provide", function($provide) {
 var PLURAL_CATEGORY = {ZERO: "zero", ONE: "one", TWO: "two", FEW: "few", MANY: "many", OTHER: "other"};
+function getDecimals(n) {
+  n = n + '';
+  var i = n.indexOf('.');
+  return (i == -1) ? 0 : n.length - i - 1;
+}
+
+function getVF(n, opt_precision) {
+  var v = opt_precision;
+
+  if (undefined === v) {
+    v = Math.min(getDecimals(n), 3);
+  }
+
+  var base = Math.pow(10, v);
+  var f = ((n * base) | 0) % base;
+  return {v: v, f: f};
+}
+
 $provide.value("$locale", {
   "DATETIME_FORMATS": {
     "AMPMS": [
-      "\u0bae\u0bc1\u0bb1\u0bcd\u0baa\u0b95\u0bb2\u0bcd",
-      "\u0baa\u0bbf\u0bb1\u0bcd\u0baa\u0b95\u0bb2\u0bcd"
+      "AM",
+      "PM"
     ],
     "DAY": [
-      "\u0b9e\u0bbe\u0baf\u0bbf\u0bb1\u0bc1",
-      "\u0ba4\u0bbf\u0b99\u0bcd\u0b95\u0bb3\u0bcd",
-      "\u0b9a\u0bc6\u0bb5\u0bcd\u0bb5\u0bbe\u0baf\u0bcd",
-      "\u0baa\u0bc1\u0ba4\u0ba9\u0bcd",
-      "\u0bb5\u0bbf\u0baf\u0bbe\u0bb4\u0ba9\u0bcd",
-      "\u0bb5\u0bc6\u0bb3\u0bcd\u0bb3\u0bbf",
-      "\u0b9a\u0ba9\u0bbf"
+      "\u0d1e\u0d3e\u0d2f\u0d31\u0d3e\u0d34\u0d4d\u200c\u0d1a",
+      "\u0d24\u0d3f\u0d19\u0d4d\u0d15\u0d33\u0d3e\u0d34\u0d4d\u200c\u0d1a",
+      "\u0d1a\u0d4a\u0d35\u0d4d\u0d35\u0d3e\u0d34\u0d4d\u0d1a",
+      "\u0d2c\u0d41\u0d27\u0d28\u0d3e\u0d34\u0d4d\u200c\u0d1a",
+      "\u0d35\u0d4d\u0d2f\u0d3e\u0d34\u0d3e\u0d34\u0d4d\u200c\u0d1a",
+      "\u0d35\u0d46\u0d33\u0d4d\u0d33\u0d3f\u0d2f\u0d3e\u0d34\u0d4d\u200c\u0d1a",
+      "\u0d36\u0d28\u0d3f\u0d2f\u0d3e\u0d34\u0d4d\u200c\u0d1a"
     ],
     "ERANAMES": [
-      "\u0b95\u0bbf\u0bb1\u0bbf\u0bb8\u0bcd\u0ba4\u0bc1\u0bb5\u0bc1\u0b95\u0bcd\u0b95\u0bc1 \u0bae\u0bc1\u0ba9\u0bcd",
-      "\u0b85\u0ba9\u0bcb \u0b9f\u0bcb\u0bae\u0bbf\u0ba9\u0bbf"
+      "\u0d15\u0d4d\u0d30\u0d3f\u0d38\u0d4d\u200c\u0d24\u0d41\u0d35\u0d3f\u0d28\u0d4d \u0d2e\u0d41\u0d2e\u0d4d\u0d2a\u0d4d",
+      "\u0d06\u0d28\u0d4d\u0d28\u0d4b \u0d21\u0d4a\u0d2e\u0d3f\u0d28\u0d3f"
     ],
     "ERAS": [
-      "\u0b95\u0bbf.\u0bae\u0bc1.",
-      "\u0b95\u0bbf.\u0baa\u0bbf."
+      "\u0d15\u0d4d\u0d30\u0d3f.\u0d2e\u0d41.",
+      "\u0d0e\u0d21\u0d3f"
     ],
     "FIRSTDAYOFWEEK": 6,
     "MONTH": [
-      "\u0b9c\u0ba9\u0bb5\u0bb0\u0bbf",
-      "\u0baa\u0bbf\u0baa\u0bcd\u0bb0\u0bb5\u0bb0\u0bbf",
-      "\u0bae\u0bbe\u0bb0\u0bcd\u0b9a\u0bcd",
-      "\u0b8f\u0baa\u0bcd\u0bb0\u0bb2\u0bcd",
-      "\u0bae\u0bc7",
-      "\u0b9c\u0bc2\u0ba9\u0bcd",
-      "\u0b9c\u0bc2\u0bb2\u0bc8",
-      "\u0b86\u0b95\u0bb8\u0bcd\u0b9f\u0bcd",
-      "\u0b9a\u0bc6\u0baa\u0bcd\u0b9f\u0bae\u0bcd\u0baa\u0bb0\u0bcd",
-      "\u0b85\u0b95\u0bcd\u0b9f\u0bcb\u0baa\u0bb0\u0bcd",
-      "\u0ba8\u0bb5\u0bae\u0bcd\u0baa\u0bb0\u0bcd",
-      "\u0b9f\u0bbf\u0b9a\u0bae\u0bcd\u0baa\u0bb0\u0bcd"
+      "\u0d1c\u0d28\u0d41\u0d35\u0d30\u0d3f",
+      "\u0d2b\u0d46\u0d2c\u0d4d\u0d30\u0d41\u0d35\u0d30\u0d3f",
+      "\u0d2e\u0d3e\u0d7c\u0d1a\u0d4d\u0d1a\u0d4d",
+      "\u0d0f\u0d2a\u0d4d\u0d30\u0d3f\u0d7d",
+      "\u0d2e\u0d47\u0d2f\u0d4d",
+      "\u0d1c\u0d42\u0d7a",
+      "\u0d1c\u0d42\u0d32\u0d48",
+      "\u0d13\u0d17\u0d38\u0d4d\u0d31\u0d4d\u0d31\u0d4d",
+      "\u0d38\u0d46\u0d2a\u0d4d\u0d31\u0d4d\u0d31\u0d02\u0d2c\u0d7c",
+      "\u0d12\u0d15\u0d4d\u200c\u0d1f\u0d4b\u0d2c\u0d7c",
+      "\u0d28\u0d35\u0d02\u0d2c\u0d7c",
+      "\u0d21\u0d3f\u0d38\u0d02\u0d2c\u0d7c"
     ],
     "SHORTDAY": [
-      "\u0b9e\u0bbe",
-      "\u0ba4\u0bbf",
-      "\u0b9a\u0bc6",
-      "\u0baa\u0bc1",
-      "\u0bb5\u0bbf",
-      "\u0bb5\u0bc6",
-      "\u0b9a"
+      "Sun",
+      "Mon",
+      "Tue",
+      "Wed",
+      "Thu",
+      "Fri",
+      "Sat"
     ],
     "SHORTMONTH": [
-      "\u0b9c\u0ba9.",
-      "\u0baa\u0bbf\u0baa\u0bcd.",
-      "\u0bae\u0bbe\u0bb0\u0bcd.",
-      "\u0b8f\u0baa\u0bcd.",
-      "\u0bae\u0bc7",
-      "\u0b9c\u0bc2\u0ba9\u0bcd",
-      "\u0b9c\u0bc2\u0bb2\u0bc8",
-      "\u0b86\u0b95.",
-      "\u0b9a\u0bc6\u0baa\u0bcd.",
-      "\u0b85\u0b95\u0bcd.",
-      "\u0ba8\u0bb5.",
-      "\u0b9f\u0bbf\u0b9a."
+      "\u0d1c\u0d28\u0d41",
+      "\u0d2b\u0d46\u0d2c\u0d4d\u0d30\u0d41",
+      "\u0d2e\u0d3e\u0d7c",
+      "\u0d0f\u0d2a\u0d4d\u0d30\u0d3f",
+      "\u0d2e\u0d47\u0d2f\u0d4d",
+      "\u0d1c\u0d42\u0d7a",
+      "\u0d1c\u0d42\u0d32\u0d48",
+      "\u0d13\u0d17",
+      "\u0d38\u0d46\u0d2a\u0d4d\u0d31\u0d4d\u0d31\u0d02",
+      "\u0d12\u0d15\u0d4d\u0d1f\u0d4b",
+      "\u0d28\u0d35\u0d02",
+      "\u0d21\u0d3f\u0d38\u0d02"
     ],
     "STANDALONEMONTH": [
-      "\u0b9c\u0ba9\u0bb5\u0bb0\u0bbf",
-      "\u0baa\u0bbf\u0baa\u0bcd\u0bb0\u0bb5\u0bb0\u0bbf",
-      "\u0bae\u0bbe\u0bb0\u0bcd\u0b9a\u0bcd",
-      "\u0b8f\u0baa\u0bcd\u0bb0\u0bb2\u0bcd",
-      "\u0bae\u0bc7",
-      "\u0b9c\u0bc2\u0ba9\u0bcd",
-      "\u0b9c\u0bc2\u0bb2\u0bc8",
-      "\u0b86\u0b95\u0bb8\u0bcd\u0b9f\u0bc1",
-      "\u0b9a\u0bc6\u0baa\u0bcd\u0b9f\u0bae\u0bcd\u0baa\u0bb0\u0bcd",
-      "\u0b85\u0b95\u0bcd\u0b9f\u0bcb\u0baa\u0bb0\u0bcd",
-      "\u0ba8\u0bb5\u0bae\u0bcd\u0baa\u0bb0\u0bcd",
-      "\u0b9f\u0bbf\u0b9a\u0bae\u0bcd\u0baa\u0bb0\u0bcd"
+      "\u0d1c\u0d28\u0d41\u0d35\u0d30\u0d3f",
+      "\u0d2b\u0d46\u0d2c\u0d4d\u0d30\u0d41\u0d35\u0d30\u0d3f",
+      "\u0d2e\u0d3e\u0d7c\u0d1a\u0d4d\u0d1a\u0d4d",
+      "\u0d0f\u0d2a\u0d4d\u0d30\u0d3f\u0d7d",
+      "\u0d2e\u0d47\u0d2f\u0d4d",
+      "\u0d1c\u0d42\u0d7a",
+      "\u0d1c\u0d42\u0d32\u0d48",
+      "\u0d13\u0d17\u0d38\u0d4d\u0d31\u0d4d\u0d31\u0d4d",
+      "\u0d38\u0d46\u0d2a\u0d4d\u0d31\u0d4d\u0d31\u0d02\u0d2c\u0d7c",
+      "\u0d12\u0d15\u0d4d\u200c\u0d1f\u0d4b\u0d2c\u0d7c",
+      "\u0d28\u0d35\u0d02\u0d2c\u0d7c",
+      "\u0d21\u0d3f\u0d38\u0d02\u0d2c\u0d7c"
     ],
     "WEEKENDRANGE": [
-      6,
+      5,
       6
     ],
-    "fullDate": "EEEE, d MMMM, y",
-    "longDate": "d MMMM, y",
-    "medium": "d MMM, y h:mm:ss a",
-    "mediumDate": "d MMM, y",
+    "fullDate": "EEEE, MMMM d, y",
+    "longDate": "MMMM d, y",
+    "medium": "MMM d, y h:mm:ss a",
+    "mediumDate": "MMM d, y",
     "mediumTime": "h:mm:ss a",
-    "short": "d-M-yy h:mm a",
-    "shortDate": "d-M-yy",
+    "short": "M/d/yy h:mm a",
+    "shortDate": "M/d/yy",
     "shortTime": "h:mm a"
   },
   "NUMBER_FORMATS": {
-    "CURRENCY_SYM": "\u20b9",
+    "CURRENCY_SYM": "$",
     "DECIMAL_SEP": ".",
     "GROUP_SEP": ",",
     "PATTERNS": [
       {
-        "gSize": 2,
+        "gSize": 3,
         "lgSize": 3,
         "maxFrac": 3,
         "minFrac": 0,
@@ -106,20 +124,20 @@ $provide.value("$locale", {
         "posSuf": ""
       },
       {
-        "gSize": 2,
+        "gSize": 3,
         "lgSize": 3,
         "maxFrac": 2,
         "minFrac": 2,
         "minInt": 1,
-        "negPre": "-\u00a4\u00a0",
+        "negPre": "-\u00a4",
         "negSuf": "",
-        "posPre": "\u00a4\u00a0",
+        "posPre": "\u00a4",
         "posSuf": ""
       }
     ]
   },
-  "id": "ta",
-  "localeID": "ta",
-  "pluralCat": function(n, opt_precision) {  if (n == 1) {    return PLURAL_CATEGORY.ONE;  }  return PLURAL_CATEGORY.OTHER;}
+  "id": "en",
+  "localeID": "en",
+  "pluralCat": function(n, opt_precision) {  var i = n | 0;  var vf = getVF(n, opt_precision);  if (i == 1 && vf.v == 0) {    return PLURAL_CATEGORY.ONE;  }  return PLURAL_CATEGORY.OTHER;}
 });
 }]);
